e4bff2490dff047f440b63da529a654e9cef3780
Your Name <09taumang@gmail.com>
2023-04-21 10:46:54 +0200

    Still trying to figure out how to render the user's inputs

--- backend/index.js
+++ backend/index.js
@@ -18,20 +18,25 @@ app.post(`/home_form_index`, (req,res)=>{
         message
     } = req.body
 
+    if(!name|| !email|| !message){
+        res.status(400).send('Please provide a name, email, and a message.')
+        return
+    }
+
     const transporter = nodemailer.createTransport({
-        service:'Gmail',
+        service:'gmail',
         auth:{
             user:'kagsiho@gmail.com',
             pass:'09Taumang',
         },
     })
 
-    const mailOptions = {
-        from:'kagishoph@gmail.com',
-        to:'09taumang@gmail.com',
-        subject:'New form submission from BITS website',
-        text:`Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-    }
+    // const mailOptions = {
+    //     from:'kagishoph@gmail.com',
+    //     to:'09taumang@gmail.com',
+    //     subject:'New form submission from BITS website',
+    //     text:`Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    // }
 
     transporter.sendMail(mailOptions,(error,info)=>{
         if (error){  
@@ -39,7 +44,16 @@ app.post(`/home_form_index`, (req,res)=>{
         res.status(500).send('error:could not send email')
         }else{
          console.log('Email sent:',info.response)
-         res.send('Form submitted successfully');
+         res.send(`
+         <section id="success">
+            <div class="inner">
+                <header>
+                    <h2>Message Sent</h2>
+                    <p>Your message has been sent successfully. We will get back to you soon.</p>
+                </header>
+            </div>
+        </section>
+         `);
         }
     })
 })
