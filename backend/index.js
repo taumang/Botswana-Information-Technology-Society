b5599dd8e00fd80e64352c97ef8263737dbfcd3e
Your Name <09taumang@gmail.com>
2023-04-26 11:11:02 +0200

    commented out the route iof form

--- backend/index.js
+++ backend/index.js
@@ -4,47 +4,47 @@ const nodemailer = require('nodemailer')
 
 const app = express()
 
-app.use(bodyParser.urlencoded(({extended:false})))
+app.use(bodyParser.urlencoded(({ extended: false })))
 app.use(bodyParser.json());
 
-app.get(`/`,(req,res)=>{
+app.get(`/`, (req, res) => {
     res.sendFile(__dirname + '../index.html')
 })
 
-app.post(`/home_form_index`, (req,res)=>{
+app.post(`/`, (req, res) => {
     const {
         name,
         email,
         message
     } = req.body
 
-    if(!name|| !email|| !message){
+    if (!name || !email || !message) {
         res.status(400).send('Please provide a name, email, and a message.')
         return
     }
 
     const transporter = nodemailer.createTransport({
-        service:'gmail',
-        auth:{
-            user:'kagsihoph@gmail.com',
-            pass:'obzjkzrfonxcmfrb',
+        service: 'gmail',
+        auth: {
+            user: 'kagsihoph@gmail.com',
+            pass: 'obzjkzrfonxcmfrb',
         },
     })
 
     const mailOptions = {
-         from:'kagishoph@gmail.com',
-         to: document.getElementById('email').value,
-         subject:'New form submission from BITS website',
-         text:`Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-     }
+        from: 'kagishoph@gmail.com',
+        to: document.getElementById('email').value,
+        subject: 'New form submission from BITS website',
+        text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+    }
 
-    transporter.sendMail(mailOptions,(error,info)=>{
-        if (error){  
-        // console.log(error) 
-        res.status(500).send('error:could not send email')
-        }else{
-        //  console.log('Email sent:',info.response)
-         res.send(`
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            // console.log(error) 
+            res.status(500).send('error:could not send email')
+        } else {
+            //  console.log('Email sent:',info.response)
+            res.send(`
          <section id="success">
             <div class="inner">
                 <header>
@@ -59,6 +59,6 @@ app.post(`/home_form_index`, (req,res)=>{
 })
 
 const port = process.env.PORT || 3800
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server is live on ${port}`)
 })
