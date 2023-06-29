2bbeb6652c15baf76730f8c97cb6fc17e1a96cd1
Kagisho Taumang Phahlamohlaka <47403327+taumang@users.noreply.github.com>
2023-06-29 11:13:56 +0000

    moved the file to the root

--- backend/index.js
+++
@@ -1,2 +0,0 @@
-//https://botswana-information-technology-society.vercel.app/
-
---
+++ index.js
@@ -0,0 +1,25 @@
+//
+
+const zlib = require('zlib');
+const fs = require('fs');
+
+const directoryPath = './https://botswana-information-technology-society.vercel.app/';
+
+const htmlFiles = fs.readdirSync(directoryPath).filter(file => file.endsWith('.html'));
+
+htmlFiles.forEach(file=>{
+    const filePath = `${directoryPath}/${file}`;
+    const html = fs.readFileSync(filePath);
+
+    zlib.gzip(html,(error,compressedData)=>{
+        if(error){
+            console.error(`Compression error for file ${file}`,error);
+            return;
+        }
+
+        const compressedFilePath = `${filePath}.gz`;
+        fs.writeFileSync(compressedFilePath,compressedData);
+
+        console.log(`Compression successful for file ${file}`)
+    })
+})
